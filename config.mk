9db591c61e15cf1c6e3c99a7dfbb17efe2bef5b1
Louie S <louie@example.com>
2024-07-12 18:05:20 -0400

    Modularize for use with Dash Docset Builder

--- config.mk
+++ config.mk
@@ -1,56 +1,16 @@
-DOCSET_NAME = GNU_Bison
-
-DOCSET_DIR    = $(DOCSET_NAME).docset
-CONTENTS_DIR  = $(DOCSET_DIR)/Contents
-RESOURCES_DIR = $(CONTENTS_DIR)/Resources
-DOCUMENTS_DIR = $(RESOURCES_DIR)/Documents
-
-INFO_PLIST_FILE = $(CONTENTS_DIR)/Info.plist
-INDEX_FILE      = $(RESOURCES_DIR)/docSet.dsidx
-ICON_FILE       = $(DOCSET_DIR)/icon.png
-ARCHIVE_FILE    = $(DOCSET_NAME).tgz
+SRC_ICON_FILE=$(SOURCE_DIR)/icon.png
 
 MANUAL_URL  = https://www.gnu.org/software/bison/manual/bison.html_node.tar.gz
 MANUAL_FILE = tmp/bison.html_node.tar.gz
 
-DOCSET = $(INFO_PLIST_FILE) $(INDEX_FILE) $(ICON_FILE)
-
-all: $(DOCSET)
-
-archive: $(ARCHIVE_FILE)
-
-clean:
-	rm -rf $(DOCSET_DIR) $(ARCHIVE_FILE)
-
-tmp:
-	mkdir -p $@
-
-$(ARCHIVE_FILE): $(DOCSET)
-	tar --exclude='.DS_Store' -czf $@ $(DOCSET_DIR)
-
 $(MANUAL_FILE): tmp
 	curl -o $@ $(MANUAL_URL)
-
-$(DOCSET_DIR):
-	mkdir -p $@
-
-$(CONTENTS_DIR): $(DOCSET_DIR)
-	mkdir -p $@
-
-$(RESOURCES_DIR): $(CONTENTS_DIR)
-	mkdir -p $@
 
 $(DOCUMENTS_DIR): $(RESOURCES_DIR) $(MANUAL_FILE)
 	mkdir -p $@
 	tar -x -z -f $(MANUAL_FILE) -C $@
 
-$(INFO_PLIST_FILE): src/Info.plist $(CONTENTS_DIR)
-	cp src/Info.plist $@
-
-$(INDEX_FILE): src/index.sh $(DOCUMENTS_DIR)
+$(INDEX_FILE): $(SOURCE_DIR)/src/index-pages.sh $(SCRIPTS_DIR)/gnu/index-terms.sh $(DOCUMENTS_DIR)
 	rm -f $@
-	src/index.sh $@ $(DOCUMENTS_DIR)/*.html
-	src/index.sh -i $@ $(DOCUMENTS_DIR)/Index-of-Terms.html
-
-$(ICON_FILE): src/icon.png $(DOCSET_DIR)
-	cp src/icon.png $@
+	$(SOURCE_DIR)/src/index-pages.sh $@ $(DOCUMENTS_DIR)/*.html
+	$(SCRIPTS_DIR)/gnu/index-terms.sh "Entry" $@ $(DOCUMENTS_DIR)/Index-of-Terms.html
---
+++ src/index-pages.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+# shellcheck source=../../../scripts/create_table.sh
+. "$(dirname "$0")"/../../../scripts/create_table.sh
+# shellcheck source=../../../scripts/get_title.sh
+. "$(dirname "$0")"/../../../scripts/get_title.sh
+# shellcheck source=../../../scripts/insert.sh
+. "$(dirname "$0")"/../../../scripts/insert.sh
+
+#get_title() {
+#	FILE="$1"
+#
+#	pup -p -f "$FILE" 'title text{}' | \
+#		sed 's/(Bison.*)//g' | \
+#		sed 's/\"/\"\"/g'
+#}
+
+DB_PATH="$1"
+shift
+
+insert_pages() {
+	# Get title and insert into table for each html file
+	while [ -n "$1" ]; do
+		unset PAGE_NAME
+		unset PAGE_TYPE
+		PAGE_NAME="$(get_title "$1" | sed 's/(Bison.*)//g')"
+		PAGE_TYPE="Guide"
+		if [ -n "$PAGE_NAME" ]; then
+			insert "$DB_PATH" "$PAGE_NAME" "$PAGE_TYPE" "$(basename "$1")"
+		fi
+		shift
+	done
+}
+
+create_table "$DB_PATH"
+insert_pages "$@"
