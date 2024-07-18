e3665a5e562206c5d91e4c584cec888bfb2b76b2
Louie S <louie@example.com>
2024-07-18 18:29:33 -0400

    Accomodate modification to index-terms.sh

--- config.mk
+++ config.mk
@@ -10,7 +10,7 @@ $(DOCUMENTS_DIR): $(RESOURCES_DIR) $(MANUAL_FILE)
 	mkdir -p $@
 	tar -x -z -f $(MANUAL_FILE) -C $@
 
-$(INDEX_FILE): $(SOURCE_DIR)/src/index-pages.sh $(SCRIPTS_DIR)/gnu/index-terms.sh $(DOCUMENTS_DIR)
+$(INDEX_FILE): $(SOURCE_DIR)/src/index-pages.sh $(SCRIPTS_DIR)/gnu/index-terms-colon.sh $(DOCUMENTS_DIR)
 	rm -f $@
 	$(SOURCE_DIR)/src/index-pages.sh $@ $(DOCUMENTS_DIR)/*.html
-	$(SCRIPTS_DIR)/gnu/index-terms.sh "Entry" $@ $(DOCUMENTS_DIR)/Index-of-Terms.html
+	$(SCRIPTS_DIR)/gnu/index-terms-colon.sh "Entry" $@ $(DOCUMENTS_DIR)/Index-of-Terms.html
